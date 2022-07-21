d26a146356fd38586d7406f08176eeb9206be446
root <root@gamestop-nfts.infrastructure.carolyn>
2022-07-21 23:26:22 +0000

    update csv and rotate logs

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ cd /root/gamestop-nft-data/
 
 git pull origin main
 
-/usr/bin/python3 /root/gamestop-nft-data/gs_nft_api.py
+#/usr/bin/python3 /root/gamestop-nft-data/gs_nft_api.py
+/usr/bin/python3 /root/gamestop-nft-data/individual_collections.py
 
 git add .
 git commit -m "update csv and rotate logs"
